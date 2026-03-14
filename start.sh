36bcb57f2cc8e19735adf8605126253a57f8d72e
Handala Mude <anwarmudey@gmail.com>
2026-03-14 15:50:46 +0200

    add procfile for railway

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
+gunicorn BackendNoordin.wsgi:application --bind 0.0.0.0:8000
